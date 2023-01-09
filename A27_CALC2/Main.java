4b777a1733188f535ec30d01b75a3b731b11bf08
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-01-09 14:58:19 +0100

    no message

---
+++ A27_CALC2/Main.java
@@ -0,0 +1,50 @@
+//
+// https://pl.spoj.com/problems/CALC2/
+// Kalkulator2
+//
+package latweA.Strona1.A27_CALC2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] rejestr = new int[10];
+        Arrays.fill(rejestr, 0);
+
+        int suma;
+        while (scanner.hasNext()) {
+
+            String znak = scanner.next();
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            if (znak.equals("z")) {
+                rejestr[a] = b;
+            } else {
+                switch (znak) {
+                    case "+":
+                        suma = rejestr[a] + rejestr[b];
+                        break;
+                    case "-":
+                        suma = rejestr[a] - rejestr[b];
+                        break;
+                    case "*":
+                        suma = rejestr[a] * rejestr[b];
+                        break;
+                    case "/":
+                        suma = rejestr[a] / rejestr[b];
+                        break;
+                    case "%":
+                        suma = rejestr[a] % rejestr[b];
+                        break;
+                    default:
+                        return;
+                }
+                System.out.println(suma);
+            }
+        }
+    }
+}
