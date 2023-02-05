2367fdbdd238c67a997f5dc6c2bfd7538538c5c6
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-02-05 17:25:17 +0100

    no message

---
+++ A52_WSEGA/Main.java
@@ -0,0 +1,27 @@
+//
+// https://pl.spoj.com/problems/WSEGA/
+// Wiek segmentolka
+//
+package latweA.Strona2.A52_WSEGA;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int tests = scanner.nextInt();
+
+        while (tests > 0) {
+            int numberOfSegments = scanner.nextInt();
+            int buffer = 0;
+
+            for (int i = 0; i < numberOfSegments; i++) {
+                buffer += scanner.nextInt();
+            }
+            System.out.println(buffer + (numberOfSegments - 1));
+
+            tests--;
+        }
+    }
+}
