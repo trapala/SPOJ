2af2d0fb9bd71b7077451d7e63f940b68a4583e5
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-02-19 18:50:20 +0100

    no message

--- A09_PP0502B/Main.java
+++ A09_PP0502B/Main.java
@@ -7,22 +7,22 @@ package latweA.Strona1.A09_PP0502B;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      int tests = Integer.parseInt(scanner.nextLine());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tests = Integer.parseInt(scanner.nextLine());
 
-      for (int i = 0; i < tests; i++) {
-          int n = scanner.nextInt();
-          int[] arrayInt = new int[n];
+        for (int i = 0; i < tests; i++) {
+            int n = scanner.nextInt();
+            int[] arrayInt = new int[n];
 
-          for (int j = 0; j < n; j++) {
-              arrayInt[j] = scanner.nextInt();
-          }
+            for (int j = 0; j < n; j++) {
+                arrayInt[j] = scanner.nextInt();
+            }
 
-          for (int k = n-1; k >= 0; k--) {
-              System.out.print(arrayInt[k] + " ");
-          }
-          System.out.println();
-      }
-  }
+            for (int k = n - 1; k >= 0; k--) {
+                System.out.print(arrayInt[k] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
