cdb604141b9924997a70e37812da4a5438067c42
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-02-19 18:51:27 +0100

    no message

--- A02_FCTRL3/Main.java
+++ A02_FCTRL3/Main.java
@@ -8,30 +8,30 @@ package latweA.Strona1.A02_FCTRL3;
 import java.util.Scanner;
 
 public class Main {
-    private static int [] factorionNumbers(int num) {
-        switch (num){
+    private static int[] factorionNumbers(int num) {
+        switch (num) {
             case 0:
             case 1:
-                return new int [] {0,1};
+                return new int[]{0, 1};
             case 2:
-                return new int [] {0,2};
+                return new int[]{0, 2};
             case 5:
             case 6:
             case 8:
-                return new int[] {2,0};
+                return new int[]{2, 0};
             case 3:
-                return new int[] {0,6};
+                return new int[]{0, 6};
             case 4:
-                return new int[] {2,4};
+                return new int[]{2, 4};
             case 7:
-                return new int[] {4,0};
+                return new int[]{4, 0};
             case 9:
-                return new int[] {8,0};
+                return new int[]{8, 0};
             default:
-                return new int[] {0,0};
+                return new int[]{0, 0};
         }
     }
-	
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int tests;
@@ -39,7 +39,7 @@ public class Main {
         tests = scanner.nextInt();
         for (int i = 0; i < tests; i++) {
             number = scanner.nextInt();
-            int [] result = factorionNumbers(number);
+            int[] result = factorionNumbers(number);
             System.out.println(result[0] + " " + result[1]);
         }
     }
