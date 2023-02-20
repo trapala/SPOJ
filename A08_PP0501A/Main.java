4f6b2ca2cf195591daadb66aadf567ac10b29ba0
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-02-20 20:47:54 +0100

    no message

--- A08_PP0501A/Main.java
+++ A08_PP0501A/Main.java
@@ -8,25 +8,25 @@ package latweA.Strona1.A08_PP0501A;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-      int firstNumber, secondNumber;
+    public static void main(String[] args) {
+        int firstNumber, secondNumber;
 
-      Scanner scanner = new Scanner(System.in);
-      int tests = Integer.parseInt(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        int tests = Integer.parseInt(scanner.nextLine());
 
-      for (int i = 0; i < tests; i++) {
-          String[] line = scanner.nextLine().split(" ");
-          firstNumber = Integer.parseInt(line[0]);
-          secondNumber = Integer.parseInt(line[1]);
+        for (int i = 0; i < tests; i++) {
+            String[] line = scanner.nextLine().split(" ");
+            firstNumber = Integer.parseInt(line[0]);
+            secondNumber = Integer.parseInt(line[1]);
 
-          System.out.println(NWD(firstNumber, secondNumber));
-      }
-  }
+            System.out.println(NWD(firstNumber, secondNumber));
+        }
+    }
 
-    static int NWD (int a, int b) {
+    static int NWD(int a, int b) {
         if (b == 0)
             return a;
         else
-            return NWD(b, a%b);
+            return NWD(b, a % b);
     }
 }
