1280f8e053ee2eb49c7fa8a8f348accd81c1474f
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-02-22 20:08:42 +0100

    no message

--- A11_PP0504B/Main.java
+++ A11_PP0504B/Main.java
@@ -2,6 +2,7 @@
 // https://pl.spoj.com/problems/PP0504B/
 // StringMerge
 //
+
 package latweA.Strona1.A11_PP0504B;
 
 import java.util.Scanner;
@@ -13,25 +14,25 @@ public class Main {
         int tests = scanner.nextInt();
 
         for (int i = 0; i < tests; i++) {
-           String word1 = scanner.next();
-           String word2 = scanner.next();
+            String word1 = scanner.next();
+            String word2 = scanner.next();
 
-          char[] charArray1 = word1.toCharArray();
-          char[] charArray2 = word2.toCharArray();
+            char[] charArray1 = word1.toCharArray();
+            char[] charArray2 = word2.toCharArray();
 
             int licznik = word2.length();
             if (word2.length() > word1.length()) {
                 licznik = word1.length();
             }
 
-            char[] charArrays = new char[licznik*2];
-          int a = 0;
-          int b = 0;
-            for (int k = 0; k < licznik; k++ ) {
+            char[] charArrays = new char[licznik * 2];
+            int a = 0;
+            int b = 0;
+            for (int k = 0; k < licznik; k++) {
                 charArrays[a] = charArray1[b];
-                charArrays[a+1] = charArray2[b];
+                charArrays[a + 1] = charArray2[b];
                 a += 2;
-                b ++;
+                b++;
             }
             System.out.println(charArrays);
         }
