bb2a3df9dc7f44cd20b9a06e212208d7436b9249
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-03-10 20:49:25 +0100

    no message

--- A30_SYS/Main.java
+++ A30_SYS/Main.java
@@ -2,6 +2,7 @@
 // https://pl.spoj.com/problems/SYS/
 // Systemy pozycyjne
 //
+
 package latweA.Strona1.A30_SYS;
 
 import java.util.Scanner;
@@ -17,22 +18,19 @@ public class Main {
             t--;
         }
     }
+
     private static String system11(int liczba) {
         // cyfry:0,1,2,3,4,5,6,7,8,9,A
         StringBuilder sb = new StringBuilder();
         while (liczba != 0) {
             if (liczba % 11 <= 9) {
                 sb.append(liczba % 11);
-                // System.out.println("if: " + sb);
             } else {
                 sb.append('A');
-                //  System.out.println("else: " + sb);
             }
             liczba = liczba / 11;
-            // System.out.println("liczba: " + liczba);
         }
         sb.reverse();
-        // System.out.println("reverse: " + sb);
         return sb.toString();
     }
 }
