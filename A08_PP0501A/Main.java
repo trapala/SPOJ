ec0ce6f8b314de4f15beb5d7da5f7c640e44b89d
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-04-14 19:43:44 +0200

    no message

--- A08_PP0501A/Main.java
+++ A08_PP0501A/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int firstNumber, secondNumber;
-
         Scanner scanner = new Scanner(System.in);
         int tests = Integer.parseInt(scanner.nextLine());
 
