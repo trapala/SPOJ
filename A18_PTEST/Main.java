c019018d208e3bb6423963a1f538b22e0505370d
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-04-27 19:32:01 +0200

    no message

--- A18_PTEST/Main.java
+++ A18_PTEST/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
