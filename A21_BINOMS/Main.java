9a3f66930539d3c69c4706e6fe5743e04a67d1ec
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-04-30 20:51:21 +0200

    no message

--- A21_BINOMS/Main.java
+++ A21_BINOMS/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int tests = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < tests; i++) {
