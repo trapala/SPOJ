cafd86943403f195385a77b49116965251d5b32d
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-05-08 19:41:36 +0200

    no message

--- A28_POL/Main.java
+++ A28_POL/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int howMany = scanner.nextInt();
 
         for (int i = 0; i < howMany; i++) {
