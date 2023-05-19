a57849a5f42915f3d4e3ab548a26530f2bebe55e
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-05-19 11:40:53 +0200

    no message

--- A01_PRIME_T/Main.java
+++ A01_PRIME_T/Main.java
@@ -5,32 +5,38 @@
 
 package latweA.Strona1.A01_PRIME_T;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws java.lang.Exception {
-        Scanner scanner1 = new Scanner(System.in);
-        int tests = scanner1.nextInt();
-        int[] input = new int[tests];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < tests; i++) {
-            input[i] = scanner1.nextInt();
+        int n = scanner.nextInt(); // liczba testów
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            numbers.add(scanner.nextInt()); // wczytaj liczby
         }
 
-        for (int i = 0; i < tests; i++) {
-            System.out.println(isPrime(input[i]));
-        }
+        numbers.stream()
+                .map(Main::isPrime)
+                .forEach(isPrime -> System.out.println(isPrime ? "TAK" : "NIE"));
     }
 
-    public static String isPrime(int n) {
-        if (n == 1) {
-            return "NIE";
+    // Metoda sprawdzająca, czy liczba jest liczbą pierwszą
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
         }
-        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return "NIE";
+
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
             }
         }
-        return "TAK";
+
+        return true;
     }
 }
