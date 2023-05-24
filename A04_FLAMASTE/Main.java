d06ca2e2f8e6d2db815023296bb51cfe7cb2583d
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-05-24 21:25:24 +0200

    complete

--- A04_FLAMASTE/Main.java
+++ A04_FLAMASTE/Main.java
@@ -7,6 +7,9 @@ package A04_FLAMASTE;
 
 import java.util.Scanner;
 
+/**
+ * Program kompresuje ciąg znaków poprzez zamianę kolejnych powtarzających się liter na literę oraz liczbę ich powtórzeń.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -47,3 +50,13 @@ public class Main {
         return compressed.toString();
     }
 }
+
+/*
+ Program wczytuje liczbę testów, a następnie dla każdego testu wczytuje ciąg znaków.
+ Algorytm polega na kompresowaniu ciągu znaków poprzez zamianę kolejnych powtarzających się liter na literę oraz liczbę ich powtórzeń.
+ Program wykorzystuje metodę compressWord, która przyjmuje słowo wejściowe i zwraca skompresowane słowo.
+ W metodzie compressWord, na podstawie podanego słowa, iteruje się po jego znakach, zliczając powtarzające się znaki.
+ Gdy napotkany znak jest inny od poprzedniego, aktualne zliczenie i odpowiednie znaki są dołączane do skompresowanego słowa.
+ Jeśli liczba powtórzeń danego znaku wynosi 2 lub więcej, dodawana jest również sama litera.
+ Ostatecznie, skompresowane słowo jest zwracane jako wynik. Kompresowane słowo jest wyświetlane na standardowym wyjściu.
+ */
