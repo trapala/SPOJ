8a660a92cd29ddb8c48dfa3c703085708848a878
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-05-25 11:04:35 +0200

    complete

--- A04_FLAMASTE/Main.java
+++ A04_FLAMASTE/Main.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int testCount = scanner.nextInt();
         scanner.nextLine(); // Pominięcie znaku nowej linii
 
