44fecfefbb47044841dcd552fef5baf0bebf6de9
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-05-31 19:42:00 +0200

    complete

--- A10_ETI06F1/Main.java
+++ A10_ETI06F1/Main.java
@@ -1,28 +1,54 @@
-//
-// https://pl.spoj.com/problems/ETI06F1/
-// Pole pewnego koła
-//
+/*
+ *
+ * https://pl.spoj.com/problems/ETI06F1/
+ * Pole pewnego koła
+ */
 
-package latweA.Strona1.A10_ETI06F1;
+package A10_ETI06F1;
 
 import java.util.Scanner;
 
+/**
+ * Program obliczający pole koła na podstawie promienia i odległości między środkami sfer.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double r = scanner.nextDouble();
-        double d = scanner.nextDouble();
+        double radius = scanner.nextDouble(); // promień sfery
+        double distance = scanner.nextDouble(); // odległość między środkami sfer
 
         double pi = 3.141592654;
 
-        double r2 = Math.pow(r, 2);
-        double d2 = Math.pow(d, 2) / 4;
-        double output = (r2 - d2) * pi;
-
-        output *= 100;
-        output = Math.round(output);
-        output /= 100;
+        double output = calculateCircleArea(radius, distance, pi);
         System.out.println(output);
+
+        scanner.close();
+    }
+
+    /**
+     * Metoda obliczająca pole koła na podstawie promienia i odległości między środkami sfer.
+     *
+     * @param radius   promień sfery
+     * @param distance odległość między środkami sfer
+     * @param pi       wartość liczby pi
+     * @return pole koła
+     */
+    public static double calculateCircleArea(double radius, double distance, double pi) {
+        double radiusSquared = Math.pow(radius, 2);
+        double distanceSquared = Math.pow(distance, 2) / 4;
+        double area = (radiusSquared - distanceSquared) * pi;
+        area = Math.round(area * 100) / 100.0; // zaokrąglenie do dwóch miejsc po przecinku
+
+        return area;
     }
 }
+
+/*
+* Program oblicza pola koła na podstawie podanego promienia (radius) i odległości między środkami sfer (distance).
+* Algorytm polega na wczytaniu promienia i odległości z wejścia, a następnie wykorzystaniu tych wartości do obliczenia pola koła.
+* Wartość liczby pi jest przyjęta jako 3.141592654.
+* Metoda calculateCircleArea przyjmuje promień, odległość i wartość liczby pi jako parametry,
+* a następnie wykonuje obliczenia na podstawie tych danych.
+* Wynik, reprezentujący pole koła, jest zaokrąglany do dwóch miejsc po przecinku i wypisywany na standardowym wyjściu.
+ */
