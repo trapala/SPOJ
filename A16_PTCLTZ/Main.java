9ca7b9f2eb11a55cbc381289cf5ea9a6e89409c7
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-06-29 17:58:25 +0200

    complete

--- A16_PTCLTZ/Main.java
+++ A16_PTCLTZ/Main.java
@@ -5,37 +5,73 @@
 
 package A16_PTCLTZ;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Program oblicza pierwszą wartość n, dla której xn=1 w ciągu rekurencyjnym.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int tests = scanner.nextInt();
+        int t = scanner.nextInt(); // liczba testów
 
-        for (int i = 0; i < tests; i++) {
-            int s = scanner.nextInt();
-            int n;
+        List<Case> cases = new ArrayList<>();
 
-            if (s == 1) {
-                n = 0;
-            } else {
-                n = 0;
-                while (s != 1) {
-                    if (s % 2 == 0) { // parzyste
-                        s = s / 2;
-                        System.out.println("Parzyste s: " + s);
-                        n++;
-                        System.out.println("Parzyste n: " + n);
-                    }
-                    if (s % 2 != 0 && s > 1) {
-                        s = 3 * s + 1;
-                        System.out.println("Nieparzyste s: " + s);
-                        n++;
-                        System.out.println("Nieparzyste n: " + n);
-                    }
-                }
-            }
+        // Wczytywanie danych testowych
+        for (int i = 0; i < t; i++) {
+            int s = scanner.nextInt(); // wartość początkowa ciągu xn
+            cases.add(new Case(s));
+        }
+
+        // Obliczanie wartości n i wyświetlanie wyników
+        for (Case c : cases) {
+            int n = c.calculateN();
             System.out.println(n);
         }
     }
 }
+
+/**
+ * Klasa reprezentująca pojedynczy przypadek testowy.
+ */
+class Case {
+    private int s;
+
+    /**
+     * Tworzy nowy obiekt klasy Case.
+     *
+     * @param s wartość początkowa ciągu xn.
+     */
+    public Case(int s) {
+        this.s = s;
+    }
+
+    /**
+     * Oblicza pierwszą wartość n, dla której xn=1.
+     *
+     * @return wartość n.
+     */
+    public int calculateN() {
+        int n = 0;
+        int x = s;
+        while (x != 1) {
+            if (x % 2 == 0) {
+                x /= 2;
+            } else {
+                x = 3 * x + 1;
+            }
+            n++;
+        }
+        return n;
+    }
+}
+
+/*
+ * Program oblicza pierwszą wartość n, dla której xn=1 w ciągu rekurencyjnym.
+ * Algorytm polega na iteracyjnym obliczaniu kolejnych wartości ciągu xn na podstawie parzystości aktualnej wartości.
+ * Wykorzystana jest klasa Case, która reprezentuje pojedynczy przypadek testowy.
+ * Metoda calculateN() oblicza wartość n dla danego przypadku.
+ * Wyniki są wyświetlane na standardowym wyjściu za pomocą metody
+ */
