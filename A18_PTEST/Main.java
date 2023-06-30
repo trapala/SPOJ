664a844647e564e2d23a1147148332a79e9be0e7
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2023-06-30 18:49:39 +0200

    complete

--- A18_PTEST/Main.java
+++ A18_PTEST/Main.java
@@ -3,17 +3,28 @@
  * Zadanie próbne
  */
 
-
 package A18_PTEST;
 
 import java.util.Scanner;
 
+/**
+ * Program dodaje dwie liczby całkowite.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Wczytywanie liczb a i b
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
+        // Obliczanie sumy a + b i wyświetlenie wyniku
         System.out.println(a + b);
     }
 }
+
+/*
+ * Program dodaje dwie liczby całkowite.
+ * Algorytm polega na wczytaniu dwóch liczb z wejścia, a następnie obliczeniu ich sumy.
+ * Wynik jest wyświetlany na standardowym wyjściu.
+ */
